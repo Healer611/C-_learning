9a8385335e82c0e98c25e6aad4326b09f1d72bf4
classmate-Han <2207946194@qq.com>
2023-11-22 01:05:21 +0800

    默认成员函数

--- Classes and objects/Part2/Part2/test.cpp
+++ Classes and objects/Part2/Part2/test.cpp
@@ -271,6 +271,74 @@ private:
 //}
 
 
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void Print()
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+	// 拷贝构造
+	// Date d2(d1);
+	Date(Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+// 传值传参
+// 内置类型，编译器可以直接拷贝
+// 自定义类型的拷贝，需要调用拷贝构造
+void Func1(Date d)
+{
+
+}
+
+// 传引用传参
+void Func2(Date& d)
+{
+
+}
+
+void Func3(Date* d)
+{
+
+}
+int main()
+{
+	Date d1(2023, 2, 3);
+	Date d2(d1);
+	d1.Print();
+	d2.Print();
+	//Date d3 = d1;
+	
+	//Date d4(&d1);
+	//Date d5 = &d1;
+
+	//Func1(d1);
+	
+	
+	//Func2(d1);
+
+	//Func3(&d1);
+
+	return 0;
+}
+//
+//
+//#include <assert.h>
 //class Date
 //{
 //public:
@@ -283,189 +351,125 @@ private:
 //
 //	// 拷贝构造
 //	// Date d2(d1);
-//	Date(Date& d)
+//	Date(const Date& d)
 //	{
+//		cout << "Date(Date& d)" << endl;
 //
-//		d._year = _year;
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//
+//		/*d._year = _year;
 //		d._month = _month;
-//		d._day = _day;
+//		d._day = _day;*/
 //	}
+//
+//	int GetMonthDay(int year, int month)
+//	{
+//		assert(month > 0 && month < 13);
+//
+//		int monthArray[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400) == 0))
+//		{
+//			return 29;
+//		}
+//		else
+//		{
+//			return monthArray[month];
+//		}
+//	}
+//
+//	// d1.GetAfterXDay(100);
+//	//Date GetAfterXDay(int x)
+//	//{
+//	///*	Date tmp;
+//	//	tmp._year = _year;
+//	//	tmp._month = _month;
+//	//	tmp._day = _day;*/
+//	//	//Date tmp(*this);
+//	//	Date tmp = *this;
+//
+//	//	tmp._day += x;
+//	//	while (tmp._day > GetMonthDay(tmp._year, tmp._month))
+//	//	{
+//	//		// 进位
+//	//		tmp._day -= GetMonthDay(tmp._year, tmp._month);
+//	//		++tmp._month;
+//	//		if (tmp._month == 13)
+//	//		{
+//	//			tmp._year++;
+//	//			tmp._month = 1;
+//	//		}
+//	//	}
+//
+//	//	return tmp;
+//	//}
+//
+//	// +
+//	Date Add(int x)
+//	{
+//		Date tmp = *this;
+//		tmp._day += x;
+//		while (tmp._day > GetMonthDay(tmp._year, tmp._month))
+//		{
+//			// 进位
+//			tmp._day -= GetMonthDay(tmp._year, tmp._month);
+//			++tmp._month;
+//			if (tmp._month == 13)
+//			{
+//				tmp._year++;
+//				tmp._month = 1;
+//			}
+//		}
+//
+//		return tmp;
+//	}
+//
+//	// +=
+//	Date& AddEqual(int x)
+//	{
+//		_day += x;
+//		while (_day > GetMonthDay(_year, _month))
+//		{
+//			// 进位
+//			_day -= GetMonthDay(_year, _month);
+//			++_month;
+//			if (_month == 13)
+//			{
+//				_year++;
+//				_month = 1;
+//			}
+//		}
+//
+//		return *this;
+//	}
+//
+//	void Print()
+//	{
+//		//cout << _year << "/" << _month << "/" << _day << endl;
+//		cout << _year << "年" << _month << "月" << _day << "日" << endl;
+//	}
+//
 //private:
 //	int _year;
 //	int _month;
 //	int _day;
 //};
 //
-//// 传值传参
-//// 内置类型，编译器可以直接拷贝
-//// 自定义类型的拷贝，需要调用拷贝构造
-//void Func1(Date d)
-//{
-//
-//}
-//
-//// 传引用传参
-//void Func2(Date& d)
-//{
-//
-//}
-//
-//void Func3(Date* d)
-//{
-//
-//}
 //int main()
 //{
 //	Date d1(2023, 2, 3);
-//	Date d2(d1);
-//	//Date d3 = d1;
-//	
-//	//Date d4(&d1);
-//	//Date d5 = &d1;
+//	Date d2 = d1.Add(100);
+//	Date d3 = d1.Add(150);
 //
-//	//Func1(d1);
-//	
-//	
-//	//Func2(d1);
+//	d1.Print();
+//	d2.Print();
+//	d3.Print();
 //
-//	//Func3(&d1);
+//	d1.AddEqual(200);
+//	d1.Print();
+//
+//	// 实现一个函数，获取多少以后的一个日期
 //
 //	return 0;
 //}
-//
-//
-#include <assert.h>
-class Date
-{
-public:
-	Date(int year = 1900, int month = 1, int day = 1)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-	}
-
-	// 拷贝构造
-	// Date d2(d1);
-	Date(const Date& d)
-	{
-		cout << "Date(Date& d)" << endl;
-
-		_year = d._year;
-		_month = d._month;
-		_day = d._day;
-
-		/*d._year = _year;
-		d._month = _month;
-		d._day = _day;*/
-	}
-
-	int GetMonthDay(int year, int month)
-	{
-		assert(month > 0 && month < 13);
-
-		int monthArray[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400) == 0))
-		{
-			return 29;
-		}
-		else
-		{
-			return monthArray[month];
-		}
-	}
-
-	// d1.GetAfterXDay(100);
-	//Date GetAfterXDay(int x)
-	//{
-	///*	Date tmp;
-	//	tmp._year = _year;
-	//	tmp._month = _month;
-	//	tmp._day = _day;*/
-	//	//Date tmp(*this);
-	//	Date tmp = *this;
-
-	//	tmp._day += x;
-	//	while (tmp._day > GetMonthDay(tmp._year, tmp._month))
-	//	{
-	//		// 进位
-	//		tmp._day -= GetMonthDay(tmp._year, tmp._month);
-	//		++tmp._month;
-	//		if (tmp._month == 13)
-	//		{
-	//			tmp._year++;
-	//			tmp._month = 1;
-	//		}
-	//	}
-
-	//	return tmp;
-	//}
-
-	// +
-	Date Add(int x)
-	{
-		Date tmp = *this;
-		tmp._day += x;
-		while (tmp._day > GetMonthDay(tmp._year, tmp._month))
-		{
-			// 进位
-			tmp._day -= GetMonthDay(tmp._year, tmp._month);
-			++tmp._month;
-			if (tmp._month == 13)
-			{
-				tmp._year++;
-				tmp._month = 1;
-			}
-		}
-
-		return tmp;
-	}
-
-	// +=
-	Date& AddEqual(int x)
-	{
-		_day += x;
-		while (_day > GetMonthDay(_year, _month))
-		{
-			// 进位
-			_day -= GetMonthDay(_year, _month);
-			++_month;
-			if (_month == 13)
-			{
-				_year++;
-				_month = 1;
-			}
-		}
-
-		return *this;
-	}
-
-	void Print()
-	{
-		//cout << _year << "/" << _month << "/" << _day << endl;
-		cout << _year << "年" << _month << "月" << _day << "日" << endl;
-	}
-
-private:
-	int _year;
-	int _month;
-	int _day;
-};
-
-int main()
-{
-	Date d1(2023, 2, 3);
-	Date d2 = d1.Add(100);
-	Date d3 = d1.Add(150);
-
-	d1.Print();
-	d2.Print();
-	d3.Print();
-
-	d1.AddEqual(200);
-	d1.Print();
-
-	// 实现一个函数，获取多少以后的一个日期
-
-	return 0;
-}
