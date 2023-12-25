485f8ea630770f206a2f220ec8d2fdd92ee40b31
classmate-Han <2207946194@qq.com>
2023-12-25 20:33:07 +0800

    模拟实现stack

---
+++ stack&queue/stack/stack/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <list>
+#include "Stack.h"
+#include "Queue.h"
+int main()
+{
+	hbr::test_stack();
+	hbr::test_queue();
+	return 0;
+}
